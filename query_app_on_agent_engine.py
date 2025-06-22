7b38632b38881bf90a7fd59f8d199c184a4c350a
Jun Li <yewei369@gmail.com>
2025-06-22 19:36:33 +0000

    drop unused scripts

--- query_app_on_agent_engine.py
+++ query_app_on_agent_engine.py
@@ -9,14 +9,14 @@ from vertexai import agent_engines
 
 # Load environment variables and initialize Vertex AI
 load_dotenv()
-project_id = os.environ["GOOGLE_CLOUD_PROJECT"]
-location = os.environ["GOOGLE_CLOUD_LOCATION"]
-app_name = os.environ.get("APP_NAME", "Transcript Summarizer")
+project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
+location = os.getenv("GOOGLE_CLOUD_LOCATION")
+app_name = os.environ.get("APP_NAME", "Agent App")
 bucket_name = f"gs://{project_id}-bucket"
 
 # Initialize Google Cloud Logging with the correct project ID
 cloud_logging_client = google.cloud.logging.Client(project=project_id)
-handler = CloudLoggingHandler(cloud_logging_client, name="transcript-summarizer")
+handler = CloudLoggingHandler(cloud_logging_client, name="telco_agent")
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger().addHandler(handler)
 
@@ -36,20 +36,15 @@ logging.info(f"Using remote app: {remote_app.display_name}")
 # Get a session for the remote app
 remote_session = remote_app.create_session(user_id="u_456")
 
-transcript_to_summarize = """
-    Virtual Agent: Hi, I am a vehicle sales agent. How can I help you?
-    User: I'd like to buy a boat.
-    Virtual Agent: A big boat, or a small boat?
-    User: How much boat will $50,000 get me?
-    Virtual Agent: That will get you a very nice boat.
-    User: Let's do it!
+greetings = """
+    Hi
 """
 
 # Run the agent with this hard-coded input
 events = remote_app.stream_query(
     user_id="u_456",
     session_id=remote_session["id"],
-    message=transcript_to_summarize,
+    message=greetings,
 )
 
 # Print responses
